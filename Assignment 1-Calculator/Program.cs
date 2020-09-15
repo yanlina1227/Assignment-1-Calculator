4440132d1510c5efca5f591f0ce2886d464f3a3e
lina yan <yanlina1227@hotmail.com>
2020-09-15 14:18:45 +0200

    update before red line fix

---
+++ Assignment 1-Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Assignment_1_Calculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's work on math!");
+            bool keepLooping = true;
+            do
+            {
+                Console.Write("---selection-----\nEnter 1 to run Addition.\nEnter 2 to run Subtraction.\nEnter 3 to run Division.\nEnter 4 to run Multiplication.");
+                string userInput = Console.ReadLine(); 
+                switch (userInput)
+                {
+                Random z = new Random();
+                int a = z.Next(8);
+                int b = z.Next(6);
+
+                    case " 1 ":
+                        Console.WriteLine("result is:" + (a + b));
+                        break;
+                    case " 2 ":
+                        Console.WriteLine("result is:" + (a - b));
+                        break;
+                    case "3 ":
+                        Console.WriteLine("result is:" + (a * b));
+                        break;
+                    case " 4 ":
+                        Console.WriteLine("result is:" + (a / b));
+                        break;
+                        if (a / b) = 0, 
+
+                }
+            }while (keepLooping);
+        }
+   
+
+
+
+
+
+    
+}
+ 
